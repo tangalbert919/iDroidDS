e1aa8c3e30bc6120952f5b3354c1c2dcc9ff68d7
Albert Tang <tangalbert919@gmail.com>
2017-02-01 18:10:46 -0600

    Experimental (mostly dangerous) upgrades
    I just updated TinyXML and AsmJIT to reduce the memory footprint and upgrade performance.
    Also, verison 48 (unofficial) is now in the works.

--- README.md
+++ README.md
@@ -4,9 +4,11 @@
 
 nds4droid is a free (including ad-free) open source Nintendo DS emulator for Android smartphones. You can find it on the [Google Play Store](https://play.google.com/store/apps/details?id=com.opendoorstudios.ds4droid&hl=en). Do not try to install this on iOS because iOS does not accept or understand anything written in Java.
 
-All releases from [jquesnelle](https://github.com/jquesnelle) will come from him. All releases from this fork is not uploaded to the play store, but can be found in the releases section of this fork.
+All releases from [jquesnelle](https://github.com/jquesnelle) will come from him. All releases from this fork is not uploaded to the play store, but can be found in the releases section of this fork. APKs will be uploaded to the release section of this fork if I believe I need to.
 
-This emulator uses the [DeSmuME](http://desmume.org) 0.9.9 threaded interpreter (that is old), but also uses Lightning JIT if you want faster performance. Support for 64-bit devices will be available soon.
+The current version is 48 beta 1 on this fork.
+
+This emulator uses the [DeSmuME](http://desmume.org) 0.9.10 threaded interpreter (which is old), but also uses Lightning JIT if you want faster performance. Support for 64-bit devices will be available soon (although I doubt that's needed, regardless of performance).
 
 ### Issues
 
@@ -14,6 +16,12 @@ If you found an issue, please [make an issue ticket here](https://github.com/tan
 * Has nothing to do with this emulator,
 * Contains copyrighted stuff,
 * or has something to do with adding support for unsupported devices.
+
+### To-do list
+
+* Increase performance overall.
+* Upgrade to DeSmuME 0.9.11.
+* Update libraries to the latest version (if applicable).
 
 **DISCLAIMER:** nds4droid is licensed under the GNU General Public License, which means the software is provided as-is. The Nintendo DS, and the Nintendo DS logo are trademarks of Nintendo, Inc. Games that support the Nintendo DS are trademarks of Nintendo, Inc., and their respective companies and affiliates.
 
--- app/src/main/jni/desmume/src/utils/AsmJit/x86/x86builder.h
+++ app/src/main/jni/desmume/src/utils/AsmJit/x86/x86builder.h
@@ -5,14 +5,17 @@
 // Zlib - See LICENSE.md file in the package.
 
 // [Guard]
-#ifndef _ASMJIT_X86_X86ASSEMBLER_H
-#define _ASMJIT_X86_X86ASSEMBLER_H
+#ifndef _ASMJIT_X86_X86BUILDER_H
+#define _ASMJIT_X86_X86BUILDER_H
+
+#include "../asmjit_build.h"
+#if !defined(ASMJIT_DISABLE_BUILDER)
 
 // [Dependencies]
-#include "../base/assembler.h"
-#include "../base/utils.h"
+#include "../base/codebuilder.h"
+#include "../base/simdtypes.h"
 #include "../x86/x86emitter.h"
-#include "../x86/x86operand.h"
+#include "../x86/x86misc.h"
 
 // [Api-Begin]
 #include "../asmjit_apibegin.h"
@@ -23,25 +26,26 @@ namespace asmjit {
 //! \{
 
 // ============================================================================
-// [asmjit::X86Assembler]
+// [asmjit::CodeBuilder]
 // ============================================================================
 
-//! X86/X64 assembler.
-//!
-//! X86/X64 assembler emits machine-code into buffers managed by \ref CodeHolder.
-class ASMJIT_VIRTAPI X86Assembler
-  : public Assembler,
-    public X86EmitterImplicitT<X86Assembler> {
+//! Architecture-dependent \ref CodeBuilder targeting X86 and X64.
+class ASMJIT_VIRTAPI X86Builder
+  : public CodeBuilder,
+    public X86EmitterImplicitT<X86Builder> {
 
 public:
-  typedef Assembler Base;
+  ASMJIT_NONCOPYABLE(X86Builder)
+  typedef CodeBuilder Base;
 
   // --------------------------------------------------------------------------
   // [Construction / Destruction]
   // --------------------------------------------------------------------------
 
-  ASMJIT_API X86Assembler(CodeHolder* code = nullptr) noexcept;
-  ASMJIT_API virtual ~X86Assembler() noexcept;
+  //! Create a `X86Builder` instance.
+  ASMJIT_API X86Builder(CodeHolder* code = nullptr) noexcept;
+  //! Destroy the `X86Builder` instance.
+  ASMJIT_API ~X86Builder() noexcept;
 
   // --------------------------------------------------------------------------
   // [Compatibility]
@@ -58,29 +62,16 @@ public:
   ASMJIT_INLINE operator const X86Emitter&() const noexcept { return *asEmitter(); }
 
   // --------------------------------------------------------------------------
-  // [Accessors]
-  // --------------------------------------------------------------------------
-
-  // NOTE: X86Assembler uses _privateData to store 'address-override' bit that
-  // is used to decide whether to emit address-override (67H) prefix based on
-  // the memory BASE+INDEX registers. It's either `kX86MemInfo_67H_X86` or
-  // `kX86MemInfo_67H_X64`.
-  ASMJIT_INLINE uint32_t _getAddressOverrideMask() const noexcept { return _privateData; }
-  ASMJIT_INLINE void _setAddressOverrideMask(uint32_t m) noexcept { _privateData = m; }
-
-  // --------------------------------------------------------------------------
   // [Events]
   // --------------------------------------------------------------------------
 
-  ASMJIT_API Error onAttach(CodeHolder* code) noexcept override;
-  ASMJIT_API Error onDetach(CodeHolder* code) noexcept override;
+  ASMJIT_API virtual Error onAttach(CodeHolder* code) noexcept override;
 
   // --------------------------------------------------------------------------
   // [Code-Generation]
   // --------------------------------------------------------------------------
 
-  ASMJIT_API Error _emit(uint32_t instId, const Operand_& o0, const Operand_& o1, const Operand_& o2, const Operand_& o3) override;
-  ASMJIT_API Error align(uint32_t mode, uint32_t alignment) override;
+  ASMJIT_API virtual Error _emit(uint32_t instId, const Operand_& o0, const Operand_& o1, const Operand_& o2, const Operand_& o3) override;
 };
 
 //! \}
@@ -91,4 +82,5 @@ public:
 #include "../asmjit_apiend.h"
 
 // [Guard]
-#endif // _ASMJIT_X86_X86ASSEMBLER_H
+#endif // !ASMJIT_DISABLE_BUILDER
+#endif // _ASMJIT_X86_X86BUILDER_H
