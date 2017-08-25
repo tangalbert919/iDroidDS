5c6bdd6ee3c7c7f04a59c84720a588e40022191e
Albert Tang <tangalbert919@gmail.com>
2017-08-24 22:12:51 -0500

    Add DeSmuME 0.9.11 core

---
+++ app/src/main/jni/desmume-0.9.11/src/utils/lightning/lightning_arm.mk
@@ -0,0 +1,26 @@
+# Android ndk file for nds4droid
+# Lightning JIT 2.1.0. Makefile built by Albert Tang.
+
+LOCAL_PATH := $(call my-dir)
+MY_LOCAL_PATH := $(LOCAL_PATH)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE     := liblightning-v7
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/lib
+LOCAL_SRC_FILES  := lib/jit_arm.c \
+                    lib/jit_arm-cpu.c \
+                    lib/jit_arm-swf.c \
+                    lib/jit_arm-sz.c \
+                    lib/jit_arm-vtp.c \
+                    lib/jit_memory.c \
+                    lib/jit_names.c \
+                    lib/jit_note.c \
+                    lib/jit_print.c \
+                    lib/jit_size.c \
+                    lib/lightning.c
+
+LOCAL_ARM_NEON 			:= false
+LOCAL_ARM_MODE          := arm
+LOCAL_CFLAGS            := -marm -march=armv7-a -mfloat-abi=softfp
+
+include $(BUILD_SHARED_LIBRARY)
