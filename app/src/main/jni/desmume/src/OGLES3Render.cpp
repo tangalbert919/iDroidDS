e167abbb8414f83d6fca9a1bb73b4457235bb122
Albert Tang <tangalbert919@gmail.com>
2017-08-30 20:38:45 -0500

    Start working on OpenGL ES 3.0 Renderer

--- app/src/main/jni/desmume/src/OGLES3Render.cpp
+++ app/src/main/jni/desmume/src/OGLES3Render.cpp
@@ -1,4 +1,34 @@
-//
-// Created by Albert Tang on 8/26/2017.
-//
+/*
+	Copyright (C) 2017 The nds4droid Team
 
+	This file is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 2 of the License, or
+	(at your option) any later version.
+
+	This file is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with the this software.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "OGLES3Render.h"
+
+#include <stdlib.h>
+#include <string.h>
+
+#include "debug.h"
+#include "gfx3d.h"
+#include "NDSSystem.h"
+#include "texcache.h"
+
+typedef struct {
+    unsigned int major;
+    unsigned int minor;
+} OGLVersion;
+
+static OGLVersion _OGLDriverVersion = {0, 0};
+//static OpenGLESRenderer *_OGLRenderer = NULL;
--- app/src/main/jni/desmume/src/OGLES3Render.h
+++ app/src/main/jni/desmume/src/OGLES3Render.h
@@ -1,8 +1,47 @@
-//
-// Created by Albert Tang on 8/26/2017.
-//
+/*
+	Copyright (C) 2017 The nds4droid Team
 
-#ifndef NDS4DROID_OGLES3RENDER_H
-#define NDS4DROID_OGLES3RENDER_H
+	This file is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 2 of the License, or
+	(at your option) any later version.
 
-#endif //NDS4DROID_OGLES3RENDER_H
+	This file is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with the this software.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+#ifndef OGLES3RENDER_H
+#define OGLES3RENDER_H
+
+#include <queue>
+#include <set>
+#include <string>
+#include "render3D.h"
+#include "types.h"
+
+#include <GLES3/gl3.h>
+#include <GLES3/gl3ext.h>
+#include <EGL/egl.h>
+
+#define OGLEXT(procPtr, func)		procPtr func = NULL;
+#define INITOGLEXT(procPtr, func)	func = (procPtr)eglGetProcAddress(#func);
+#define EXTERNOGLEXT(procPtr, func)	extern procPtr func;
+
+// VAO
+EXTERNOGLEXT(PFNGLGENVERTEXARRAYSPROC, glGenVertexArrays)
+EXTERNOGLEXT(PFNGLDELETEVERTEXARRAYSPROC, glDeleteVertexArrays)
+EXTERNOGLEXT(PFNGLBINDVERTEXARRAYPROC, glBindVertexArray)
+
+// VBO
+EXTERNOGLEXT(PFNGLMAPBUFFERRANGEPROC, glMapBufferRange)
+EXTERNOGLEXT(PFNGLUNMAPBUFFERPROC, glUnmapBuffer)
+EXTERNOGLEXT(PFNGLBINDBUFFERPROC, glBindBuffer)
+
+
+#endif //OGLES3RENDER_H
