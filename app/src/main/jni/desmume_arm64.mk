052d855d7374c037478cfca2b4d24778cfb29ad8
tangalbert919 <tangalbert919@gmail.com>
2018-06-06 22:54:19 -0500

    Simplify arm64 makefile, let's see if it works

--- app/src/main/jni/desmume_arm64.mk
+++ app/src/main/jni/desmume_arm64.mk
@@ -13,63 +13,15 @@ LOCAL_C_INCLUDES		:= 	$(LOCAL_PATH)/desmume/src \
 							$(LOCAL_PATH)/desmume/src/android/7z/CPP \
 							$(LOCAL_PATH)/desmume/src/android/7z/CPP/include_windows
 						   
-LOCAL_SRC_FILES			:= 	desmume/src/addons/slot1_none.cpp \
-							desmume/src/addons/slot1_r4.cpp \
-							desmume/src/addons/slot1_retail_auto.cpp \
-							desmume/src/addons/slot1_retail_mcrom.cpp \
-							desmume/src/addons/slot1_retail_mcrom_debug.cpp \
-							desmume/src/addons/slot1_retail_nand.cpp \
-							desmume/src/addons/slot1comp_mc.cpp \
-							desmume/src/addons/slot1comp_protocol.cpp \
-							desmume/src/addons/slot1comp_rom.cpp \
-							desmume/src/addons/slot2_auto.cpp \
-							desmume/src/addons/slot2_expMemory.cpp \
-							desmume/src/addons/slot2_gbagame.cpp \
-							desmume/src/addons/slot2_guitarGrip.cpp \
-							desmume/src/addons/slot2_mpcf.cpp \
-							desmume/src/addons/slot2_none.cpp \
-							desmume/src/addons/slot2_paddle.cpp \
-							desmume/src/addons/slot2_passme.cpp \
-							desmume/src/addons/slot2_piano.cpp \
-							desmume/src/addons/slot2_rumblepak.cpp \
-							desmume/src/utils/decrypt/crc.cpp \
-							desmume/src/utils/decrypt/decrypt.cpp \
-							desmume/src/utils/decrypt/header.cpp \
-							desmume/src/utils/libfat/cache.cpp \
-							desmume/src/utils/libfat/directory.cpp \
-							desmume/src/utils/libfat/disc.cpp \
-							desmume/src/utils/libfat/fatfile.cpp \
-							desmume/src/utils/libfat/fatdir.cpp \
-							desmume/src/utils/libfat/file_allocation_table.cpp \
-							desmume/src/utils/libfat/filetime.cpp \
-							desmume/src/utils/libfat/libfat.cpp \
-							desmume/src/utils/libfat/libfat_public_api.cpp \
-							desmume/src/utils/libfat/lock.cpp \
-							desmume/src/utils/libfat/partition.cpp \
-							desmume/src/utils/tinyxml/tinyxml.cpp \
-							desmume/src/utils/tinyxml/tinyxmlparser.cpp \
-							desmume/src/utils/tinyxml/tinyxmlerror.cpp \
-							desmume/src/utils/tinyxml/tinystr.cpp \
+LOCAL_SRC_FILES			:= 	desmume/src/addons/*.cpp \
+							desmume/src/utils/decrypt/*.cpp \
+							desmume/src/utils/libfat/*.cpp \
+							desmume/src/utils/tinyxml/*.cpp \
 							desmume/src/utils/advanscene.cpp \
 							desmume/src/utils/ConvertUTF.c \
-							desmume/src/utils/datetime.cpp \
-							desmume/src/utils/dlditool.cpp \
-							desmume/src/utils/emufat.cpp \
-							desmume/src/utils/fsnitro.cpp \
-							desmume/src/utils/guid.cpp \
-							desmume/src/utils/md5.cpp \
-							desmume/src/utils/task.cpp \
-							desmume/src/utils/vfat.cpp \
-							desmume/src/utils/xstring.cpp \
+							desmume/src/utils/*.cpp \
 							desmume/src/metaspu/metaspu.cpp \
-							desmume/src/filter/2xsai.cpp \
-							desmume/src/filter/bilinear.cpp \
-							desmume/src/filter/epx.cpp \
-							desmume/src/filter/hq2x.cpp \
-							desmume/src/filter/hq4x.cpp \
-							desmume/src/filter/lq2x.cpp \
-							desmume/src/filter/scanline.cpp \
-							desmume/src/filter/xbrz.cpp \
+							desmume/src/filter/*.cpp \
 							desmume/src/arm_instructions.cpp \
 							desmume/src/armcpu.cpp \
 							desmume/src/bios.cpp \
