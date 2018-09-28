ac0cc78a3753de3145e651430dc67e5f3e5de5d1
Albert Tang <tangalbert919@gmail.com>
2018-09-27 23:04:48 -0500

    I'll take "-Ofast, updates, and shared libc++" for 800.

--- app/src/main/jni/Application.mk
+++ app/src/main/jni/Application.mk
@@ -1,12 +1,12 @@
 # Android ndk makefile for nds4droid
 
 # We need to switch to libc++
-APP_STL := c++_static
+APP_STL := c++_shared
 APP_ABI := armeabi-v7a x86 arm64-v8a x86_64
 
 # For releases. The first CFLAG line is for GCC, and the second one is Clang.
 #APP_CFLAGS := -Ofast -fmodulo-sched -fmodulo-sched-allow-regmoves -fsingle-precision-constant -fvariable-expansion-in-unroller -fomit-frame-pointer -fno-math-errno -funsafe-math-optimizations -ffinite-math-only -fdata-sections -fbranch-target-load-optimize2 -fexceptions -fno-stack-protector -fforce-addr -ftree-loop-im -ftree-loop-ivcanon -fivopts -ftree-loop-if-convert-stores -floop-strip-mine -ftree-loop-distribution -floop-interchange -ftree-loop-linear -floop-block -Wno-psabi
-APP_CFLAGS := -O2 -fmodulo-sched -fmodulo-sched-allow-regmoves -fsingle-precision-constant -fvariable-expansion-in-unroller -fomit-frame-pointer -fno-math-errno -funsafe-math-optimizations -ffinite-math-only -fdata-sections -fexceptions -fno-stack-protector -fforce-addr -fivopts
+APP_CFLAGS := -Ofast -fmodulo-sched -fmodulo-sched-allow-regmoves -fsingle-precision-constant -fvariable-expansion-in-unroller -fomit-frame-pointer -fno-math-errno -funsafe-math-optimizations -ffinite-math-only -fdata-sections -fexceptions -fno-stack-protector -fforce-addr -fivopts
 # For debugging only.
 #APP_CFLAGS := -Wno-psabi
 
