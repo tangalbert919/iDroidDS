a38c8dea6e8b965281187d9f421f863720f36d08
tangalbert919 <tangalbert919@gmail.com>
2018-10-12 22:52:41 -0500

    I have a feeling this will help increase performance

--- app/src/main/jni/desmume/src/android/main.cpp
+++ app/src/main/jni/desmume/src/android/main.cpp
@@ -591,7 +591,7 @@ void JNI(restoreState, int slot)
 
 void loadSettings(JNIEnv* env)
 {
-	CommonSettings.num_cores = sysconf( _SC_NPROCESSORS_ONLN );
+	CommonSettings.num_cores = static_cast<int>(sysconf(_SC_NPROCESSORS_ONLN ));
 	LOGI("%i cores detected", CommonSettings.num_cores); 
 	CommonSettings.cheatsDisable = GetPrivateProfileBool(env,"General", "cheatsDisable", false, IniName);
 	CommonSettings.autodetectBackupMethod = GetPrivateProfileInt(env,"General", "autoDetectMethod", 0, IniName);
